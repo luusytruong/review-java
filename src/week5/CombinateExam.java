47034206834040364e6336cbeeed56474b221818
luusytruong <lst27062004@gmail.com>
2025-01-12 18:36:02 +0700

    up

--- src/week5/CombinateExam.java
+++ src/week5/CombinateExam.java
@@ -1,6 +1,7 @@
 package week5;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 import java.awt.*;
@@ -14,12 +15,10 @@ public class CombinateExam extends JFrame {
 
     public void initComponents() {
         JPanel contentPanel = new JPanel();
-        JPanel rightPanel = new JPanel();
         JPanel databasPanel = new JPanel();
         JPanel systemPanel = new JPanel();
 
         contentPanel.setLayout(null);
-        rightPanel.setLayout(null);
         databasPanel.setLayout(null);
         systemPanel.setLayout(null);
 
@@ -29,16 +28,16 @@ public class CombinateExam extends JFrame {
         JTextField tf2 = new JTextField();
 
         lb1.setBounds(12, 12, 120, 24);
-        tf1.setBounds(12 + 120, 12, 260, 24);
+        tf1.setBounds(12 + 120, 12, 268, 24);
         lb2.setBounds(12, 12 + 24, 120, 24);
-        tf2.setBounds(12 + 120, 12 + 24, 260, 24);
+        tf2.setBounds(12 + 120, 12 + 24, 268, 24);
 
         contentPanel.add(lb1);
         contentPanel.add(tf1);
         contentPanel.add(lb2);
         contentPanel.add(tf2);
 
-        databasPanel.setBounds(12, 60 + 12, 380, 89);
+        databasPanel.setBounds(12, 60 + 12, 388, 89);
         TitledBorder titledBorder = BorderFactory.createTitledBorder("Database");
         databasPanel.setBorder(titledBorder);
 
@@ -49,10 +48,10 @@ public class CombinateExam extends JFrame {
         JButton btn4 = new JButton("Compact");
 
         lb3.setBounds(12, 24, 120, 24);
-        btn1.setBounds(12 , 48+6, 80, 24);
-        btn2.setBounds(24+80 , 48+6, 80, 24);
-        btn3.setBounds(36+160 , 48+6, 80, 24);
-        btn4.setBounds(48+240 , 48+6, 80, 24);
+        btn1.setBounds(12, 48 + 6, 80, 24);
+        btn2.setBounds(24 + 80, 48 + 6, 80, 24);
+        btn3.setBounds(36 + 160, 48 + 6, 80, 24);
+        btn4.setBounds(48 + 240, 48 + 6, 88, 24);
 
         databasPanel.add(lb3);
         databasPanel.add(btn1);
@@ -62,15 +61,51 @@ public class CombinateExam extends JFrame {
 
         contentPanel.add(databasPanel);
 
-        rightPanel.setBackground(Color.lightGray);
+        systemPanel.setBounds(12, 161 + 12, 388, 120);
+        titledBorder = BorderFactory.createTitledBorder("System Database");
+        systemPanel.setBorder(titledBorder);
 
-        this.add(contentPanel, BorderLayout.CENTER);
-        this.add(rightPanel, BorderLayout.EAST);
-        // this.add(databasPanel);
-        // this.add(systemPanel);
+        ButtonGroup buttonGroup = new ButtonGroup();
+        JRadioButton jRaJRadioButton1 = new JRadioButton("None");
+        JRadioButton jRaJRadioButton2 = new JRadioButton("Database:");
+        JLabel lb4 = new JLabel("System Database...");
 
+        jRaJRadioButton1.setBounds(12, 24, 120, 24);
+        jRaJRadioButton2.setBounds(12, 48, 120, 24);
+        lb4.setBounds(388 / 2 - 90, 72 + 12, 180, 28);
+        lb4.setHorizontalAlignment(JLabel.CENTER);
+        lb4.setForeground(Color.gray);
+        lb4.setBorder(new LineBorder(Color.gray));
+
+        buttonGroup.add(jRaJRadioButton1);
+        buttonGroup.add(jRaJRadioButton2);
+        systemPanel.add(jRaJRadioButton1);
+        systemPanel.add(jRaJRadioButton2);
+        systemPanel.add(lb4);
+
+        contentPanel.add(systemPanel);
+
+        JButton btn5 = new JButton("Ok");
+        JButton btn6 = new JButton("Cancel");
+        JButton btn7 = new JButton("Help");
+        JButton btn8 = new JButton("Advanced");
+        JButton btn9 = new JButton("Options >>");
+
+        btn5.setBounds(388 + 24, 12, 120, 24);
+        btn6.setBounds(388 + 24, 12 + 24 + 12, 120, 24);
+        btn7.setBounds(388 + 24, 48 + 24 + 12, 120, 24);
+        btn8.setBounds(388 + 24, 84 + 24 + 12, 120, 24);
+        btn9.setBounds(388 + 24, 171 + 120 - 24, 120, 24);
+
+        contentPanel.add(btn5);
+        contentPanel.add(btn6);
+        contentPanel.add(btn7);
+        contentPanel.add(btn8);
+        contentPanel.add(btn9);
+
+        this.setContentPane(contentPanel);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(600, 400);
+        this.setSize(388 + 120 + 24 + 16 + 12, 173 + 120 + 24 + 26);
         this.setLocationRelativeTo(null);
     }
 }
